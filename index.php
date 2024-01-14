1acf71ac4464d2514a56aea51eff0289b16efac6
Tricia Kim <triciapany6@gmail.com>
2024-01-14 16:27:19 +0000

    added an error page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Api</title>
+</head>
+<body style="width:100dvw; height:100dvh; display:flex; flex-direction:column; justify-content:center; align-items:center; gap:10px;">
+   <h1> Something is coming soon...</h1>
+	<span> Get ready...</span>
+</body>
+</html>
